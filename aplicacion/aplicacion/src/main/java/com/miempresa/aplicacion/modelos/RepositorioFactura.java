7f053ef400d9b69e33bc118bd910d98c8a8636bf
dvargas540 <dvargas540@gmail.com>
2021-10-07 20:03:52 -0500

    Borrado archivos repetidos

--- aplicacion/aplicacion/src/main/java/com/miempresa/aplicacion/modelos/RepositorioFactura.java
+++
@@ -1,8 +0,0 @@
-package com.miempresa.aplicacion.modelos;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepositorioFactura extends CrudRepository<Factura,Long> {
-    Factura findByNumeroFactura(String numeroFactura);
-    
-}
--- aplicacion/aplicacion/src/main/java/com/miempresa/aplicacion/modelos/RepositorioVendedor.java
+++
@@ -1,7 +0,0 @@
-package com.miempresa.aplicacion.modelos;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepositorioVendedor extends CrudRepository<Vendedor,String> {
-    Vendedor findByCodVendedor(String codVendedor);  
-}
